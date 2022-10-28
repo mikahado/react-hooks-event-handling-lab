9187fb6c876ce07872781aae1fb7130ae73a52b6
Michael <mikahado@gmail.com>
2022-10-28 00:28:11 -0400

    event handling lab complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
 
+
 function Keypad (){
+
+    function passwordMsg(){
+        console.log("Entering password . . .")
+        }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={passwordMsg} />
+        </div>
     )
 }
 
